c52e8b375a5361ec7b6c55251a0c89cf4ee381c9
LilianCunanan <31613473+LilianCunanan@users.noreply.github.com>
2017-09-12 10:47:47 +0800

    lab 2

--- ExamGrades.java
+++ ExamGrades.java
@@ -1,3 +1,10 @@
+//-->double
+//computes for the highest value in this array
+/* double maxim(){
+ *   return 0;
+ * }
+ */
+
 class ExamGrades {
   double grades[];
   
@@ -6,5 +13,15 @@ class ExamGrades {
   }
   
   // TODO design a function that returns the highest grade
+  double maxim(){
+    double max = 0.0;
+    
+    for(int i=0; i < grades.length; i++){
+      if (max > grades[i]){
+        max = grades [i];
+      }
+    }
+    return max;
+  }
   
 }
